d94a9b23b31ffcd11cbb252ecd172983a0cb3a53
HarutoGitTsukushi <19921205yuki@gmail.com>
2025-11-18 12:51:24 +0000

    Add Next.js build and manifest files for development

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wjxuTyMbmp2lBCsusBrjZx4l5ggO7Gzt9el+cLaFhJY=\"\n}"
---
+++ .next/dev/static/development/_buildManifest.js
@@ -0,0 +1,12 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error",
+    "/api/spatial-id"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
